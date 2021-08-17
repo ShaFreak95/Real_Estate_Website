6307a5afeb023ce3944ab4f3de1ed6179c1d18da
ishafiq95 <hamsting95@gmail.com>
2021-08-17 23:59:09 +0800

    Modal Box Added
    Modal box should be expected to appear when user clicks either of the listings' image.

---
+++ js/main.js
@@ -0,0 +1,56 @@
+//Get the modal
+let modalBox = document.querySelector("#modal_box")
+
+//Get the image that can open the modal box, from item_1 until item_12
+document.querySelector(".img-content-1").addEventListener("click", function(){
+    modalBox.style.display = "block"
+})
+
+document.querySelector(".img-content-2").addEventListener("click", function(){
+    modalBox.style.display = "block"
+})
+
+document.querySelector(".img-content-3").addEventListener("click", function(){
+    modalBox.style.display = "block"
+})
+
+document.querySelector(".img-content-4").addEventListener("click", function(){
+    modalBox.style.display = "block"
+})
+
+document.querySelector(".img-content-5").addEventListener("click", function(){
+    modalBox.style.display = "block"
+})
+
+document.querySelector(".img-content-6").addEventListener("click", function(){
+    modalBox.style.display = "block"
+})
+
+document.querySelector(".img-content-7").addEventListener("click", function(){
+    modalBox.style.display = "block"
+})
+
+document.querySelector(".img-content-8").addEventListener("click", function(){
+    modalBox.style.display = "block"
+})
+
+document.querySelector(".img-content-9").addEventListener("click", function(){
+    modalBox.style.display = "block"
+})
+
+document.querySelector(".img-content-10").addEventListener("click", function(){
+    modalBox.style.display = "block"
+})
+
+document.querySelector(".img-content-11").addEventListener("click", function(){
+    modalBox.style.display = "block"
+})
+
+document.querySelector(".img-content-12").addEventListener("click", function(){
+    modalBox.style.display = "block"
+})
+
+//Closing the modal box after user clicks the green button
+document.querySelector("#close_btn").addEventListener("click", function() {
+    modalBox.style.display = "none"
+})
